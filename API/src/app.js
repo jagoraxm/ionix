98aac8d1aa7934f1b30b688c396fc026751164a9
jonathan.arias <jonathan.arias@lottuseducation.com>
2022-06-15 04:05:46 -0500

    se agrega ejercicio3 y swagger

--- API/src/app.js
+++ API/src/app.js
@@ -4,15 +4,31 @@ import cors from 'cors';
 import Response from './domain/response.js';
 import HttpStatus from './controller/user.controller.js';
 import userRoutes from './route/user.route.js';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'API Users Librería',
+            version: '1.0.0'
+        }
+    },
+    apis: ['app.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 dotenv.config();
 const app = express();
 app.use(cors({ origin: '*'}));
 app.use(express.json());
 
+
 console.log(process.env);
 
 app.use('/user', userRoutes)
+app.use('/apiDoc', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.get('/', (req, res) => res.send( new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Usuario API, v1.0.0', { data: {data: 'data'} }) ));
 app.all('*', (req, res) => res.status(HttpStatus.NOT_FOUND.code)
     .send( new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'La ruta no existe en el servidor') ));
--- API/src/route/user.route.js
+++ API/src/route/user.route.js
@@ -1,9 +1,22 @@
 import express from "express";
 import { getUsers, getUser, createUser, deleteUser, updateUser } from '../controller/user.controller.js';
 
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+
+
 const usersRoutes = express.Router();
 
 usersRoutes.route('/')
+    /**
+     * @swagger
+     * /user:
+     *    get:
+     *       description: Obtener todos los usuarios
+     *       responses:
+     *          200:
+     *             description: OK
+     */
     .get(getUsers)
     .post(createUser);
 
